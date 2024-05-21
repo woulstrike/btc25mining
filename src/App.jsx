602298d62d4df0e6589d4af7e637cfdcb81a7b4b
woulstrike <semen.saveshev@gmail.com>
2024-05-21 15:27:08 +0700

    123123

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import { HeaderButtons } from "./components/header/jsx/headersBtn"
 // const tg = window.Telegram.WebApp
-
+import './components/button/startFarmingSection'
+import { StartFaming } from './components/button/startFarmingSection'
 
 export default function App() {
   return (
     <>
+      <StartFaming />
       <div className="background">
         <HeaderButtons />
       </div>
---
+++ src/components/button/startFarmingSection.jsx
@@ -0,0 +1,11 @@
+import './css/startFarmingSection.css'
+
+export function StartFaming(){
+    return(
+        <>
+              <div className="container">
+                    <button className="centered-button">START FAMING</button>
+            </div>
+        </>
+    )
+}
