8dafa7cd5a30ffacb9fbd0b7bf657cce57b011bf
woulstrike <semen.saveshev@gmail.com>
2024-05-22 11:12:28 +0700

    123321

--- src/components/button/startFarmingSection.jsx
+++ src/components/button/startFarmingSection.jsx
@@ -53,7 +53,7 @@ export function StartFarmingSection() {
     <div className="container">
       <div className="counter-display">
         <img src={coinImg} alt="Icon" className="icon" />
-        <span className="counter" style={{ color: isCounting ? 'white' : 'black' }}>{Math.floor(counter)}</span>
+        <span className="counter" style={{ color: isCounting ? 'white' : 'black', fontSize: '40px', paddingBottom: '50px' }}>{Math.floor(counter)}</span>
       </div>
       <button className={`centered-button ${isCounting ? 'farming' : ''}`} onClick={handleClick} disabled={isCounting}>
         {isCounting ? 'Farming...' : 'Start Farm'}
