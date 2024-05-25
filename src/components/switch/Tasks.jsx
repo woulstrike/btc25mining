ce0b438b83d2871da7745c105973f1789b7a5d91
woulstrike <semen.saveshev@gmail.com>
2024-05-25 17:08:28 +0700

    task

--- src/components/switch/Tasks.jsx
+++ src/components/switch/Tasks.jsx
@@ -1,8 +1,15 @@
+import './css/Task.css'
+
+
 export function Tasks() {
     return (
-      <div>
-        <h1>Tasks</h1>
-        <p>Here are your tasks.</p>
-      </div>
+      <>
+          <span className="inscription">
+            <h1 style={{color: 'white'}}>TASKS</h1>
+            <p style={{color: '#00FFE0'}}>We’ll reward you immediately <br />
+            with points after each task completion</p>
+          </span>
+        
+      </>
     );
-  }
+}
