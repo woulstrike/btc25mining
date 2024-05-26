95cb1c2f31a1172de29e5760bec93305daf34077
woulstrike <semen.saveshev@gmail.com>
2024-05-26 14:28:12 +0700

    123

--- src/components/button/startFarmingSection.jsx
+++ src/components/button/startFarmingSection.jsx
@@ -1,13 +1,14 @@
-import { useState, useEffect } from 'react'
-import './css/startFarmingSection.css'
-import coinImg from '../assets/coin.png'
+import { useState, useEffect } from 'react';
+import './css/startFarmingSection.css';
+import coinImg from '../assets/coin.png';
 
 export function StartFarmingSection() {
-  const initialCounter = parseInt(localStorage.getItem('counter')) || 0;
+  const initialCounter = parseFloat(localStorage.getItem('counter')) || 0;
   const [counter, setCounter] = useState(initialCounter);
   const [isCounting, setIsCounting] = useState(false);
-  const duration = 8 * 60 * 60 * 1000; 
-  const incrementPerSecond = 57 / (8 * 60 * 60); 
+  const duration = 8 * 60 * 60 * 1000; // 8 hours in milliseconds
+  const totalIncrement = 57;
+  const incrementPerSecond = totalIncrement / (8 * 60 * 60); // Increment value for each second
 
   useEffect(() => {
     if (isCounting) {
@@ -21,7 +22,7 @@ export function StartFarmingSection() {
           localStorage.setItem('counter', newCounter);
           return newCounter;
         });
-      }, 1000); 
+      }, 1000); // Update every second
 
       return () => clearInterval(timer);
     }
@@ -40,14 +41,13 @@ export function StartFarmingSection() {
   };
 
   const formatCounter = (count) => {
-    return count.toLocaleString('en-US', {
-      minimumIntegerDigits: 9,
-      useGrouping: true,
-    }).replace(/,/g, '.');
+    const paddedCount = count.toFixed(9).replace('.', '');
+    const formattedCount = `${paddedCount.slice(0, 3)}.${paddedCount.slice(3, 6)}.${paddedCount.slice(6, 9)}`;
+    return formattedCount;
   };
 
   return (
-    <div className="container" style={{paddingBottom: '0'}}>
+    <div className="container" style={{ paddingBottom: '0' }}>
       <div className="content-wrapper">
         <div className="icon-container">
           <img src={coinImg} alt="Icon" className="icon" />
