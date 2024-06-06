cc53018aaaa8d6dfde62fd24c1780ff0726d0141
woulstrike <semen.saveshev@gmail.com>
2024-06-06 16:14:52 +0700

    123

--- bot/index.mjs
+++ bot/index.mjs
@@ -14,7 +14,7 @@ bot.on("message", async (msg) => {
     // const chat = await bot.getChat(chatId);
     // const userId = chat.from.id;
 
-    await bot.sendMessage(chatId, "Начни майнить BTC25 в этой игре!", {
+    await bot.sendMessage(chatId, "Начни майнить @BTC25 Promo в этой игре!", {
       reply_markup: {
         inline_keyboard: [
           [{ text: "Начать майнить", web_app: { url: webAppUrl } }],
